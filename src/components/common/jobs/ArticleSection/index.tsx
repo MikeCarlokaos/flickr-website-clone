fe60fcfcd3eb253bc3f3b5ba57189aeaa60b3040
Mike Carlo Gonzaga <kaosvioge@gmail.com>
2023-02-09 00:15:44 +0800

    fixed bug on Article

--- src/components/common/jobs/ArticleSection/index.tsx
+++ src/components/common/jobs/ArticleSection/index.tsx
@@ -7,7 +7,6 @@ type ArticleSectionProps = {
   coverImg: string;
   title: string;
   text: string;
-  direction: string;
   effect: string;
 };
 
@@ -15,24 +14,22 @@ const ArticleSection = ({
   coverImg,
   title,
   text,
-  direction,
   effect,
 }: ArticleSectionProps) => {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
-    <article data-aos={`${effect}`} className="w-full h-full p-10 border-b">
-      <div
-        className={`w-full h-full mx-auto flex flex-col-reverse overflow-x-hidden md:${direction}  md:w-[1060px]`}
-      >
-        <div className="w-full h-full px-20 py-5 md:w-1/2">
-          <h3 className="text-2xl font-semibold tracking-wide pb-6">{title}</h3>
-          <p>{text}</p>
-        </div>
-        <div className="w-full h-full md:w-1/2">
-          <img src={coverImg} alt="" />
-        </div>
+    <article
+      data-aos={`${effect}`}
+      className="w-full h-full p-10 border-b flex flex-col-reverse md:w-[1060px] md:mx-auto md:flex-row md:even:flex-row-reverse"
+    >
+      <div className="w-full h-full px-20 py-5 md:w-1/2">
+        <h3 className="text-2xl font-semibold tracking-wide pb-6">{title}</h3>
+        <p>{text}</p>
+      </div>
+      <div className="w-full h-full md:w-1/2">
+        <img src={coverImg} alt="" />
       </div>
     </article>
   );
